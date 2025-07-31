579ae964f3cc8c662708a3eea83447e1fb4d7d6f
orbas1 <ironcrest96@gmail.com>
2025-07-31 12:32:54 +0100

    Add multi-sig wallet contract and fix imports

---
+++ synnergy-network/cmd/smart_contracts/multi_sig_wallet.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title MultiSigWallet - simple multisig wallet example
+/// @notice Funds can only be spent after the required number of owners confirm
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint256 amount);
+    event Submit(uint indexed txId);
+    event Confirm(address indexed owner, uint indexed txId);
+    event Revoke(address indexed owner, uint indexed txId);
+    event Execute(uint indexed txId);
+
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint public numConfirmationsRequired;
+
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+    Transaction[] public transactions;
+
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "not owner");
+        _;
+    }
+
+    modifier txExists(uint _txId) {
+        require(_txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
+    modifier notConfirmed(uint _txId) {
+        require(!isConfirmed[_txId][msg.sender], "tx already confirmed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint _numConfirmationsRequired) {
+        require(_owners.length > 0, "owners required");
+        require(
+            _numConfirmationsRequired > 0 &&
+            _numConfirmationsRequired <= _owners.length,
+            "invalid number of required confirmations"
+        );
+
+        for (uint i = 0; i < _owners.length; i++) {
+            address owner = _owners[i];
+            require(owner != address(0), "invalid owner");
+            require(!isOwner[owner], "owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+        }
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function submitTransaction(address _to, uint _value, bytes calldata _data) external onlyOwner {
+        uint txId = transactions.length;
+
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            data: _data,
+            executed: false,
+            numConfirmations: 0
+        }));
+
+        emit Submit(txId);
+    }
+
+    function confirmTransaction(uint _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+        notConfirmed(_txId)
+    {
+        Transaction storage transaction = transactions[_txId];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txId][msg.sender] = true;
+
+        emit Confirm(msg.sender, _txId);
+    }
+
+    function executeTransaction(uint _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+    {
+        Transaction storage transaction = transactions[_txId];
+        require(
+            transaction.numConfirmations >= numConfirmationsRequired,
+            "cannot execute tx"
+        );
+
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(transaction.data);
+        require(success, "tx failed");
+
+        emit Execute(_txId);
+    }
+
+    function revokeConfirmation(uint _txId)
+        external
+        onlyOwner
+        txExists(_txId)
+        notExecuted(_txId)
+    {
+        require(isConfirmed[_txId][msg.sender], "tx not confirmed");
+
+        Transaction storage transaction = transactions[_txId];
+        transaction.numConfirmations -= 1;
+        isConfirmed[_txId][msg.sender] = false;
+
+        emit Revoke(msg.sender, _txId);
+    }
+}
+
