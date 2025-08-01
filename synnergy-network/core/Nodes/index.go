0408f38b3d4eb3444f5dcf87ce00de23041b6a17
orbas1 <ironcrest96@gmail.com>
2025-08-01 22:10:13 +0100

    Add gateway node type

--- synnergy-network/core/Nodes/index.go
+++ synnergy-network/core/Nodes/index.go
@@ -9,3 +9,16 @@ type NodeInterface interface {
 	Close() error
 	Peers() []string
 }
+
+// GatewayInterface extends NodeInterface with cross-chain and data functions.
+type GatewayInterface interface {
+	NodeInterface
+	ConnectChain(local, remote string) (interface{}, error)
+	DisconnectChain(id string) error
+	ListConnections() interface{}
+	RegisterExternalSource(name, url string)
+	RemoveExternalSource(name string)
+	ExternalSources() map[string]string
+	PushExternalData(name string, data []byte) error
+	QueryExternalData(name string) ([]byte, error)
+}
