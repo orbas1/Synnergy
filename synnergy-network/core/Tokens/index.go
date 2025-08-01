cbe2eba1c0352da8c1d1657eb6dfd12959b7f918
orbas1 <ironcrest96@gmail.com>
2025-08-01 20:34:45 +0100

    Add SYN600 reward token standard

--- synnergy-network/core/Tokens/index.go
+++ synnergy-network/core/Tokens/index.go
@@ -4,3 +4,14 @@ package Tokens
 type TokenInterfaces interface {
 	Meta() any
 }
+
+// RewardTokenInterface defines the extended methods of the SYN600
+// reward token standard without importing core types.
+type RewardTokenInterface interface {
+	TokenInterfaces
+	Stake(addr any, amount uint64, duration int64) error
+	Unstake(addr any) error
+	AddEngagement(addr any, points uint64) error
+	EngagementOf(addr any) uint64
+	DistributeStakingRewards(rate uint64) error
+}
