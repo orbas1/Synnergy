384c701bd6bef9fa3f06ce57bb46c484427efe5c
orbas1 <ironcrest96@gmail.com>
2025-08-01 22:20:09 +0100

    Add Warfare node type

--- synnergy-network/core/Nodes/index.go
+++ synnergy-network/core/Nodes/index.go
@@ -9,3 +9,16 @@ type NodeInterface interface {
 	Close() error
 	Peers() []string
 }
+
+// WarfareNodeInterface is implemented by nodes specialised for military
+// operations. It embeds NodeInterface and exposes additional methods defined
+// in the military_nodes subpackage.
+//
+// Keeping the interface here avoids package import cycles while allowing the
+// core package to rely on the abstract type.
+type WarfareNodeInterface interface {
+	NodeInterface
+	SecureCommand(data []byte) error
+	TrackLogistics(itemID, status string) error
+	ShareTactical(data []byte) error
+}
