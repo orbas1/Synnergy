f38dd73ae5fe89c27d4dbef4ce6a2edf47c2e397
orbas1 <ironcrest96@gmail.com>
2025-08-01 22:04:34 +0100

    Add SYN3700 index token

--- synnergy-network/core/Tokens/index.go
+++ synnergy-network/core/Tokens/index.go
@@ -1,6 +1,23 @@
 package Tokens
 
+import "time"
+
 // TokenInterfaces consolidates token standard interfaces without core deps.
 type TokenInterfaces interface {
 	Meta() any
+}
+
+// IndexComponent is a lightweight representation of an index element.
+type IndexComponent struct {
+	AssetID  uint32
+	Weight   float64
+	Quantity uint64
+}
+
+// SYN3700Interface exposes functionality for index tokens without depending on core.
+type SYN3700Interface interface {
+	TokenInterfaces
+	Components() []IndexComponent
+	MarketValue() uint64
+	LastRebalance() time.Time
 }
