0579afd8561afee1c10a9b9954f924c67b0dca14
orbas1 <ironcrest96@gmail.com>
2025-08-02 14:29:53 +0100

    chore: resolve lint conflict in content node

---
+++ synnergy-network/GUI/token-creation-tool/eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: { document: "readonly", fetch: "readonly" }
+    },
+    rules: {
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      semi: ["error", "always"]
+    }
+  }
+];
