aa713718debf30dd96a4431fb3c5a7f11eaab267
orbas1 <ironcrest96@gmail.com>
2025-08-02 16:05:04 +0100

    perf: cache env lookups

--- synnergy-network/pkg/utils/env_benchmark_test.go
+++ synnergy-network/pkg/utils/env_benchmark_test.go
@@ -1,31 +1,43 @@
 package utils
 
 import (
-    "os"
-    "testing"
+	"os"
+	"testing"
 )
 
 func BenchmarkEnvOrDefault(b *testing.B) {
-    os.Unsetenv("BENCH_KEY")
-    b.ReportAllocs()
-    for i := 0; i < b.N; i++ {
-        EnvOrDefault("BENCH_KEY", "fallback")
-    }
+	const key = "BENCH_KEY"
+	os.Setenv(key, "value")
+	clearEnvCache(key)
+	// warm cache
+	EnvOrDefault(key, "fallback")
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		EnvOrDefault(key, "fallback")
+	}
 }
 
 func BenchmarkEnvOrDefaultInt(b *testing.B) {
-    os.Setenv("BENCH_INT", "123")
-    b.ReportAllocs()
-    for i := 0; i < b.N; i++ {
-        EnvOrDefaultInt("BENCH_INT", 0)
-    }
+	const key = "BENCH_INT"
+	os.Setenv(key, "123")
+	clearEnvCache(key)
+	EnvOrDefaultInt(key, 0)
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		EnvOrDefaultInt(key, 0)
+	}
 }
 
 func BenchmarkEnvOrDefaultUint64(b *testing.B) {
-    os.Setenv("BENCH_UINT", "123")
-    b.ReportAllocs()
-    for i := 0; i < b.N; i++ {
-        EnvOrDefaultUint64("BENCH_UINT", 0)
-    }
+	const key = "BENCH_UINT"
+	os.Setenv(key, "123")
+	clearEnvCache(key)
+	EnvOrDefaultUint64(key, 0)
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		EnvOrDefaultUint64(key, 0)
+	}
 }
-
