629ffa6e257f04cb3f2a288234c3b37c5e39cf13
orbas1 <ironcrest96@gmail.com>
2025-08-03 14:12:12 +0100

    core: centralize transaction type constants

--- synnergy-network/core/transactions.go
+++ synnergy-network/core/transactions.go
@@ -296,18 +296,6 @@ func (tp *TxPool) Run(ctx context.Context) {
 	<-ctx.Done()
 }
 
-// -----------------------------------------------------------------------------
-// interfaces & stubs just to make the file compile
-// -----------------------------------------------------------------------------
-
-type TxType uint8
-
-const (
-	TxPayment TxType = iota + 1
-	TxContractCall
-	TxReversal
-)
-
 func (a *AuthoritySet) ActiveAddresses() []Address { return nil }
 
 // -----------------------------------------------------------------------------
